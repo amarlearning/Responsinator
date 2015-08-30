3abdaf2f361aa10f76ea65bb2cc7c15f0271f802
Amar Prakash Pandey <amar.om1994@gmail.com>
2015-08-30 21:40:45 +0530

    Responsinator
    It's is a web application by which you can test the responsiveness of
    your website.
    
    want to use it - http://amarpandey.ml/viewport.html

---
+++ script.js
@@ -0,0 +1,26 @@
+function small() {
+    var a = document.getElementById('name').value;
+    window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=382.40, height=750");
+}
+function medium() {
+    var a = document.getElementById('name').value;
+    window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=665.68, height=750");
+}
+function large() {
+    var a = document.getElementById('name').value;
+    window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=997.18, height=750");
+}
+function full() {
+    var a = document.getElementById('name').value;
+    window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=, height=750");
+}
+function increase() {
+    var a = document.getElementById('name').value;
+    var w= window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=750");
+    
+}
+function decrease() {
+    var a = document.getElementById('name').value;
+    var w = window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=750");
+     
+}
