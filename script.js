3a694ff182a30d97767b68f637fdd78483110cf8
Amar Prakash Pandey <amar.om1994@gmail.com>
2015-11-04 01:46:33 +0530

    Updated Responsinator

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function small() {
     var a = document.getElementById('name').value;
     window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=382.40, height=750");
@@ -14,13 +15,388 @@ function full() {
     var a = document.getElementById('name').value;
     window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=, height=750");
 }
-function increase() {
-    var a = document.getElementById('name').value;
-    var w= window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=750");
+*/
+
+$(document).ready(function(){
+    $('#small').click(function(){
+
+        $('iframe').css('width','464.44')
+    });
+});
+$(document).ready(function(){
+    $('#medium').click(function(){
+
+        $('iframe').css('width','614.70')
+    });
+});
+$(document).ready(function(){
+    $('#large').click(function(){
+
+        $('iframe').css('width','956.20')
+    });
+});
+$(document).ready(function(){
+    $('#full').click(function(){
+
+        $('iframe').css('width','1366')
+    });
+});
+
+$(document).ready(function(){
+    $('#increase').click(function(){
+
+       setTimeout( function(){
+        $('iframe').css('width','366'); 
+              },500);  
+
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','416');
+            },1000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','466');
+            },1500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','516');
+            },2000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','566');
+            },2500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','616');
+            },3000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','666');
+            },3500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','716');
+            },4000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','766');
+            },4500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','816');
+            },5000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','866');
+            },5500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','916');
+            },6000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','966');
+            },6500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1016');
+            },7000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1066');
+            },7500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1116');
+            },8000);
+       
+       setTimeout( function()
+       {
+        $('iframe').css('width','1166');
+            },8500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1216');
+            },9000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1266');
+            },9500);
+
+        setTimeout( function()
+              {
+            $('iframe').css('width','1316');
+                },10000);
+
+          setTimeout( function()
+              {
+            $('iframe').css('width','1366');
+                },10500);
     
-}
-function decrease() {
-    var a = document.getElementById('name').value;
-    var w = window.open('http://'+a, "_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=750");
-     
-}
+            
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $('#decrease').click(function(){
+
+       setTimeout( function(){
+        $('iframe').css('width','1366'); 
+              },500);  
+
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1316');
+            },1000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1266');
+            },1500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1216');
+            },2000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1166');
+            },2500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1116');
+            },3000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1066');
+            },3500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1016');
+            },4000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','966');
+            },4500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','916');
+            },5000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','866');
+            },5500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','816');
+            },6000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','766');
+            },6500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','716');
+            },7000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','666');
+            },7500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','616');
+            },8000);
+       
+       setTimeout( function()
+       {
+        $('iframe').css('width','566');
+            },8500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','516');
+            },9000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','466');
+            },9500);
+
+        setTimeout( function()
+              {
+            $('iframe').css('width','416');
+                },10000);
+
+          setTimeout( function()
+              {
+            $('iframe').css('width','366');
+                },10500);
+    
+            
+    });
+});
+
+
+
+
+
+$(document).ready(function(){
+    $('#dance').click(function(){
+
+       setTimeout( function(){
+        $('iframe').css('width','1366'); 
+              },500);  
+
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','716');
+            },1000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','466');
+            },1500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1016');
+            },2000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1266');
+            },2500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1111');
+            },3000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','611');
+            },3500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','728');
+            },4000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','350');
+            },4500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','434');
+            },5000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1350');
+            },5500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','1199');
+            },6000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','400');
+            },6500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','980');
+            },7000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','676');
+            },7500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','328');
+            },8000);
+       
+       setTimeout( function()
+       {
+        $('iframe').css('width','1250');
+            },8500);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','555');
+            },9000);
+
+       setTimeout( function()
+       {
+        $('iframe').css('width','666');
+            },9500);
+
+        setTimeout( function()
+              {
+            $('iframe').css('width','777');
+                },10000);
+
+          setTimeout( function()
+              {
+            $('iframe').css('width','888');
+                },10500);
+    
+
+          setTimeout( function()
+              {
+            $('iframe').css('width','999');
+                },11000);
+          
+          setTimeout( function()
+              {
+            $('iframe').css('width','350');
+                },11500);
+    
+          setTimeout( function()
+              {
+            $('iframe').css('width','1366');
+                },12000);
+    
+            
+    });
+});
